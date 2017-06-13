79e7d7738589704dee254140918ecd2f136e0c3a
CaguCTjkee <caguct.mac@yandex.ru>
2017-06-13 14:33:40 +0300

    Re-registration ajax-auth-script

--- wp-content/themes/weddingvendor-child/functions.php
+++ wp-content/themes/weddingvendor-child/functions.php
@@ -1,6 +1,19 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-function enqueue_parent_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+add_action('wp_enqueue_scripts', 'enqueue_parent_styles');
+function enqueue_parent_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
+
+// By Oleksii Biriukov
+// Re-registration ajax-auth-script
+function my_scripts_method()
+{
+    wp_deregister_script('ajax-auth-script');
+    wp_register_script('ajax-auth-script', get_stylesheet_directory_uri() . '/js/ajax-auth-script.js', array('jquery'));
+    wp_enqueue_script('ajax-auth-script');
+}
+
+add_action('wp_enqueue_scripts', 'my_scripts_method');
+
 ?>
